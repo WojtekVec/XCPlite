6f342533c03ed7a32344ea68829759fe8c2e933d
Przystas, Wojciech <Wojciech.Przystas@vector.com>
2025-08-13 16:02:41 +0200

    CNP-44765: Implement XCPlite for Simulink/C-RT Functions - added CMake file with the project definition of the example C-RT function DLL

---
+++ cmake/crt_function.cmake
@@ -0,0 +1,33 @@
+# === C-RT Function DLL Project ===
+
+set(CRT_DLL_TARGET_NAME crt_function_dll)
+
+file(GLOB_RECURSE crt_function_SOURCES
+    "${PROJECT_ROOT}/examples/crt_function_demo/src/*.cpp"
+    "${PROJECT_ROOT}/examples/crt_function_demo/src/*.h"
+    "${PROJECT_ROOT}/examples/crt_function_demo/src/private/*.cpp"
+    "${PROJECT_ROOT}/examples/crt_function_demo/src/private/*.h"
+    "${PROJECT_ROOT}/examples/crt_function_demo/src/shared/*.cpp"
+    "${PROJECT_ROOT}/examples/crt_function_demo/src/shared/*.h"
+    "${PROJECT_ROOT}/examples/crt_function_demo/src/shared/via/*.h"
+)
+
+add_library(${CRT_DLL_TARGET_NAME} SHARED ${crt_function_SOURCES})
+set_target_properties(${CRT_DLL_TARGET_NAME} PROPERTIES LINKER_LANGUAGE C++)
+
+target_include_directories(${CRT_DLL_TARGET_NAME} PUBLIC
+    "${PROJECT_ROOT}/examples/crt_function_demo/src"
+    "${PROJECT_ROOT}/examples/crt_function_demo/src/private"
+    "${PROJECT_ROOT}/examples/crt_function_demo/src/shared"
+    "${PROJECT_ROOT}/examples/crt_function_demo/src/shared/via"
+)
+
+if(COMMON_WARNING_FLAGS)
+    target_compile_options(${CRT_DLL_TARGET_NAME} PRIVATE ${COMMON_WARNING_FLAGS})
+endif()
+
+if(WIN32)
+    target_compile_definitions(${CRT_DLL_TARGET_NAME} PRIVATE _CRT_SECURE_NO_WARNINGS _BUILDNODELAYERDLL)
+endif()
+
+target_link_libraries(${CRT_DLL_TARGET_NAME} PRIVATE Threads::Threads)
